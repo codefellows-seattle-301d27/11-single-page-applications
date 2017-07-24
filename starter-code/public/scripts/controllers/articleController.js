56a54621e72817dfca439038ea2f5380295d7b26
la januari <lajanuari@gmail.com>
2017-07-24 16:27:22 -0700

    added show function to controller

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,10 +3,10 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
-
+  module.Article.fetchAll(module.articleView.initIndexPage);
+  $('.tab-content').show();
   module.articleController = articleController;
 })(app);
