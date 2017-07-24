bf8ecb8ba180d6c194b5661f8222c3fbe81a4a8c
Alana Franklin <alanafrankly@gmail.com>
2017-07-24 16:47:07 -0700

    Fixed issue with routes script tag

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,11 +1,11 @@
 'use strict';
 var app = app || {};
 
-page('/', articleController.initArticleSection);
-page('/about', aboutController.initAboutSection);
-page();
-
-
 // DONE: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
+page('/', app.articleController.initArticleSection);
+page('/about', app.aboutController.initAboutSection);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+
+// DONE: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+
+page();
