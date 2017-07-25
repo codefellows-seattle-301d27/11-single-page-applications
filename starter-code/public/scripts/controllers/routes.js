43b2a588af9d407c74faa9841fa7825926070937
Drew Biehle <drew.biehle@gmail.com>
2017-07-24 17:11:57 -0700

    added initArticleView function, added function to routes.js, verified that we are the bomb-diggity

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -3,11 +3,9 @@ var app = app || {};
 
 // DONE: Configure routes for this app with page.js, by registering each URL your app can handle, linked to a a single controller function to handle it. Note that these routes do not need to wrapped in an IIFE.
 // estimate 5 min, actual 3 min
+page('/', app.articleController.initArticleView);
 
-page('/', function(){
-  $('.tab-content').hide();
-})
-page('/about', aboutController.initAboutView);
+page('/about', app.aboutController.initAboutView);
 
 // DONE: What function do you call to activate page.js? Fire it off now, to execute. Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
 // estimate 5 min, actual 3 min
