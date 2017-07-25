cd94b6462686141a603cab71b986ea6d82123224
Sara Jade <wisconsinsara@gmail.com>
2017-07-24 17:07:29 -0700

    Called page() in routes.js - updated routes in routes.js

--- starter-code/public/scripts/controllers/routes.js
+++ starter-code/public/scripts/controllers/routes.js
@@ -1,18 +1,17 @@
 'use strict';
 var app = app || {};
 
-// TODO: Configure routes for this app with page.js,
+// DONE: Configure routes for this app with page.js,
 // by registering each URL your app can handle, linked to a a single controller
 // function to handle it. Note that these routes do not need to wrapped in an IIFE.
+// est 15min act 30min
 
 // for index.html
 page('/', app.articleController.initArticleController);
-// for admin.html
-// page('/admin', app.adminView.initAdminPage());
-// // for new.html
-// page('/new', app.newArticle.initNewArticlePage());
 // for about
 page('/about', app.aboutController.initAboutController);
 
-// TODO: What function do you call to activate page.js? Fire it off now, to execute.
+// DONE: What function do you call to activate page.js? Fire it off now, to execute.
 // Note that it does not need to be attached to the 'app' object nor wrapped in an IIFE.
+// est 10min act 15min
+page();
