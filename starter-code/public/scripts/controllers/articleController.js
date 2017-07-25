0239724363e1ab6b91c38b3f893b1235d72a50a9
Aeone Singson <aeone.singson@gmail.com>
2017-07-24 17:36:30 -0700

    set up articleController init to fetch all from article.js and show articles but hide everything else

--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -3,10 +3,16 @@ var app = app || {};
 
 (function(module) {
   const articleController = {};
-  // TODO: Setup a function that kicks off the fetching and rendering of articles, using the same
+  // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-
+  // Estimated 10 min | Actual 20 min
+  articleController.initArticleController = () => {
+    module.Article.fetchAll(function(){
+      $('.tab.content').hide();
+      $('#articles').show();
+    });
+  }
 
   module.articleController = articleController;
 })(app);
