3ab3f6d5cd4266dcfd94ff4ada68437d77ad8e66
britinimalloy <britini.malloy@outlook.com>
2017-07-24 18:15:02 -0700

    all pages loading properly, cleaned up dead code, changed all TODOs to DONE

--- starter-code/public/scripts/controllers/aboutController.js
+++ starter-code/public/scripts/controllers/aboutController.js
@@ -3,10 +3,8 @@ var app = app || {};
 
 (function(module) {
   const aboutController = {};
-  // let aboutController = {};
   // DONE: Define a function that hides all main section elements, and then reveals just the #about section:
   aboutController.setContent = function() {
-    //app.Article.fetchAll(app.articleView.initIndexPage);
     $('.tab-content').hide();
     $('#about').show()
   }
--- starter-code/public/scripts/controllers/articleController.js
+++ starter-code/public/scripts/controllers/articleController.js
@@ -6,11 +6,11 @@ var app = app || {};
   // DONE: Setup a function that kicks off the fetching and rendering of articles, using the same
   // code that used to be in index.html.
   // Also be sure to hide all the main section elements, and reveal the #articles section:
-  // let aboutController = {};
   articleController.setContent = function() {
-    app.Article.fetchAll(app.articleView.initIndexPage);
-    $('.tab-content').hide();
-    $('#articles').show()
+    app.Article.fetchAll(function() {
+      $('.tab-content').hide();
+      $('#articles').show()
+    });
   }
   module.articleController = articleController;
 })(app);
